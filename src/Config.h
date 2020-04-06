d106830baf1705a1d7d564a66ba72dd5f1e42124
Adrian Kuboszek <adrian.kuboszek@nokia.com>
2020-04-06 20:58:33 +0200

    Bug fixing in EEPROM data getter

--- src/Config.h
+++ src/Config.h
@@ -2,7 +2,9 @@
 #define CONFIG_H
 
 #include <Arduino.h>
+#include <ArduinoJson.h>
 #include <EEPROM.h>
+#include <FS.h>
 
 #define CONFIG_SSID_LENGTH 33
 #define CONFIG_PWD_LENGTH 33
@@ -10,7 +12,7 @@
 #define CONFIG_DB_DEVICE_ID_LENGTH 2
 #define CONFIG_DB_IP_LENGTH 16
 #define CONFIG_USERNAME_LENGTH 20
-#define CONFIG_PWD_ADDRESS 20
+#define CONFIG_DB_PWD_LENGTH 20
 
 namespace Config
 {
@@ -32,7 +34,7 @@ extern char pwd[CONFIG_PWD_LENGTH];   // wifi password
 extern char device_id[CONFIG_DB_DEVICE_ID_LENGTH]; // device ID visible in database
 extern char db_ip[CONFIG_DB_IP_LENGTH];            // IP of database
 extern char db_username[CONFIG_USERNAME_LENGTH];   // DB username
-extern char db_pwd[CONFIG_PWD_LENGTH];             // DB pasword
+extern char db_pwd[CONFIG_DB_PWD_LENGTH];          // DB pasword
 
 void load();                              // load whole data
 void load(char *target, uint8_t address); // load EEPROM data into variable
@@ -43,5 +45,4 @@ void loadDBConfig();
 bool update(Params param, const char *value); // update EEPROM variable
 int getEEPROMAddress(Params param);
 } // namespace Config
-
 #endif
