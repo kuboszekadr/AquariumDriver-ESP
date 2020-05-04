4fb2df0871eae848e430c9dc7d4a8628f5d7d359
Adrian Kuboszek <adrian.kuboszek@nokia.com>
2020-05-04 21:54:44 +0200

    Adjusted clock stretch limit to allow arduino calculate the response

--- src/I2CMaster.h
+++ src/I2CMaster.h
@@ -4,46 +4,46 @@
 #include <Arduino.h>
 #include <Wire.h>
 
-#define I2C_SLAVE_ADDRESS 8  // only one slave can be attached
-#define I2C_TIMEOUT 10  // number of trials 
-#define I2C_SLAVE_BUFFER_SIZE 32  // amount of bytes that can be sent to the slave
+#define I2C_SLAVE_ADDRESS 8      // only one slave can be attached
+#define I2C_TIMEOUT 10           // number of trials
+#define I2C_SLAVE_BUFFER_SIZE 32 // amount of bytes that can be sent to the slave
 
 #define I2C_ORDER_OFFSET 5000L
 #define I2C_REQUEST_OFFSET 10000L
 
+#define I2C_BUFFER_SIZE 1512
+
 namespace i2c
 {
-    enum Order
-    {
-        UNKNOWN = -1,
-        UPDATE_RTC,
-        UPDATE_WIFI_STATUS,  
-        WATER_CHANGE
-    };
+enum Order
+{
+    NONE = -1,
+    UPDATE_RTC,
+    WATER_CHANGE,
+    UNKNOWN = 999
+};
 
-    enum TransmissionStep
-    {
-        EMPTY = -1,  // to be set separatelly when slave is done with parsing the command
-        ONGOING,  // transmission is in progress
-        FINISHED  // all data received from the master
-    };
+enum TransmissionStep
+{
+    EMPTY = -1, // to be set separatelly when slave is done with parsing the command
+    ONGOING,    // transmission is in progress
+    FINISHED    // all data received from the master
+};
 
-    extern char buffer[512];  // maximum amount of the data that can be sent from the slave
-    // extern int requestDataLength;
-    extern TransmissionStep transmissionStep;
+extern char buffer[I2C_BUFFER_SIZE]; // maximum amount of the data that can be sent from the slave
+extern TransmissionStep transmission_step;
 
-    void begin();  // join I2C bus
+void begin();            // join I2C bus
+int requestDataLength(); // get amount of the data available by the slave
 
-    int requestDataLength();  // get amount of the data available by the slave
-    void requestData();  // starts data transmission
-    void requestData(int amount);  // starts data transmission
-    void requestDataPackage(int received_bytes, int package_size); 
+void requestData();           // starts data transmission
+void requestData(int amount); // starts data transmission
+void requestDataPackage(int received_bytes, int package_size);
+void sendOrder(char *order_msg, Order order_code); // sends order to the slave
 
-    void sendOrder();  // sends order to the slave
-    uint8_t sendDataPackage(int start, int size);
-
-    bool closeTransmission();
-}
-
+uint8_t sendDataPackage(int start, int size);
+bool closeTransmission();
+void clearBuffer();
+} // namespace i2c
 
 #endif
